66c49531759b95a1ec894facd1107a6cbc82891a
Nicolas Bettenburg <nicbet@gmail.com>
2016-09-10 17:13:42 -0400

    Pushed 0.1.1 to hex.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Essence.Mixfile do
 
   def project do
     [app: :essence,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
